c140d8798c26bf239a5add255223da79d74d675e
vikash verma <vikashraj144@gmail.com>
2018-06-22 18:13:56 +0530

    strim

--- src/com/java8/streams/Stream1.java
+++ src/com/java8/streams/Stream1.java
@@ -2,6 +2,7 @@ package com.java8.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Stream1 {
@@ -9,5 +10,10 @@ public class Stream1 {
 		List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
 		List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
 		System.out.println(filtered);
+		
+		//how to print 10 random numbers using forEach.
+		Random random = new Random();
+		random.ints().limit(10).forEach(System.out::println);
+		
 	}
 }
